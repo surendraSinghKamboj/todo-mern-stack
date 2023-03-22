b056fc386ddb033df65e730f9cb897d60ecbabd5
Surendra Singh Kamboj <116861180+surendraSinghKamboj@users.noreply.github.com>
2023-03-22 12:02:44 +0530

    postpone

--- data/connectDB.js
+++ data/connectDB.js
@@ -1,2 +1,10 @@
 import mongoose from "mongoose";
 
+export const connectDb = async () => {
+    try {
+        const status = await mongoose.connect("mongodb://127.0.0.1:27017")
+        console.log("Database conncted.........")
+    } catch (error) {
+        console.log(error)
+    }
+}
